5248269439b55c8b7ea83f6698254f4849410c21
egaraganis <vaggelhs.gkaragkanhs@gmail.com>
2024-09-18 13:01:12 +0300

    Change lume output directory, so it can match github pages deploy from branch and folder options

--- _config.ts
+++ _config.ts
@@ -1,14 +1,16 @@
-import lume from "lume/mod.ts";
-import jsx from "lume/plugins/jsx.ts";
-import favicon from "lume/plugins/favicon.ts";
+import lume from "lume/mod.ts"
+import jsx from "lume/plugins/jsx.ts"
+import favicon from "lume/plugins/favicon.ts"
 
-const site = lume();
+const site = lume({
+    dest: "./docs",
+})
 
-site.copy("_data");
-site.copy("scripts");
-site.copy("assets");
-site.loadAssets([".css"]);
-site.use(jsx());
-site.use(favicon());
+site.copy("_data")
+site.copy("scripts")
+site.copy("assets")
+site.loadAssets([".css"])
+site.use(jsx())
+site.use(favicon())
 
-export default site;
+export default site
