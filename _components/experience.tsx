aa9fe42ff0642bab26e39f0a8fec02e8f75c3d3e
egaraganis <vaggelhs.gkaragkanhs@gmail.com>
2024-11-04 12:04:41 +0200

    Update info

--- _components/experience.tsx
+++ _components/experience.tsx
@@ -14,17 +14,11 @@ helpers: Lume.Helpers
          <h3>Early 2024 - Late 2024</h3>
       </div>
 
-      <ol>
-         <li>
-            Developed two in-house backend services, Spring.
-         </li>
-         <li>
-            Integrated oAuth2.0 over Greek’s Digital Governance Information Systems (GSIS).
-         </li>
-         <li>
-            Implemented a high-level Java client over GSIS Citizen Gate REST API.
-         </li>
-      </ol>
+      <ul>
+         <li>Developed two in-house backend services, Spring.</li>
+         <li>Integrated oAuth2.0 over Greek’s Digital Governance Information Systems (GSIS).</li>
+         <li>Implemented a high-level Java client over GSIS Citizen Gate REST API.</li>
+      </ul>
    </div>
    <div class="experience-section">
       <h2>Fullstack & Data Software Engineer</h2>
@@ -33,27 +27,30 @@ helpers: Lume.Helpers
          <h3>Early 2020 - Late 2023</h3>
       </div>
 
-      <ol>
-         <li>
-            Co-implemented an in-house query language compiler, Java, Elastic, Neo4j.
-         </li>
-         <li>
-            Managed Elasticsearch’s search and analytics engine.
-         </li>
-         <li>
-            Backend development over business specific flows, Spring, Kafka.
-         </li>
-         <li>
-            Frontend development of client and server side apps, NextJS and React.
-         </li>
-         <li>
-            Release management and application deployment, Docker and Gradle.
-         </li>
+      <ul>
+         <li>Co-implemented an in-house query language compiler, Java, Elastic, Neo4j.</li>
+         <li>Managed Elasticsearch’s search and analytics engine.</li>
+         <li>Backend development over business specific flows, Spring, Kafka.</li>
+         <li>Frontend development of client and server side apps, NextJS and React.</li>
+         <li>All kind of technical writing for internal and sdk documentation.</li>
+         <li>Release management and application deployment, Docker and Gradle.</li>
          <li>
             <b>Bonus</b>: Conducted thesis on Niovity’s semantic datalake, proposing increased metadata manage-
             ment and advanced analytics.
          </li>
-      </ol>
+      </ul>
+   </div>
+   <div class="experience-section">
+      <h2>Fullstack Developer</h2>
+      <div className="step">
+         <h3>Cognihub</h3>
+         <h3>Early 2019 - Late 2024</h3>
+      </div>
+      <ul>
+         <li>
+         Fullstack development with React and NextJS.
+         </li>
+      </ul>
    </div>
 </section>
 );
